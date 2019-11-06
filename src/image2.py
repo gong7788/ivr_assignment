7153c84d6aeb2d171e71a3de955093cac0f72b80
gong7788 <Gong827029400>
2019-11-06 14:51:48 +0000

    s

---
+++ src/image2.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import roslib
+import sys
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64MultiArray, Float64
+from cv_bridge import CvBridge, CvBridgeError
+
+
+class image_converter:
+
+  # Defines publisher and subscriber
+  def __init__(self):
+    # initialize the node named image_processing
+    rospy.init_node('image_processing', anonymous=True)
+    # initialize a publisher to send images from camera2 to a topic named image_topic2
+    self.image_pub2 = rospy.Publisher("image_topic2",Image, queue_size = 1)
+    # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
+    self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw",Image,self.callback2)
+    # initialize the bridge between openCV and ROS
+    self.bridge = CvBridge()
+
+  def detect_red(self, image):
+    """Finds the position of red joint(end-effect)
+
+    :param image: matrix, width*height*3
+        The image captured by camera2
+    :return: array, [x, y]
+        The position of red joint in pixel (Top-left:[0 , 0], Right-down:[width, height])
+    """
+    mask = cv2.inRange(image, (0, 0, 100), (0, 0, 255))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((5, 5), dtype=np.uint8))
+    M = cv2.moments(mask)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    return np.array([cx, cy])
+
+  def detect_green(self, image):
+    mask = cv2.inRange(image, (0, 100, 0), (0, 255, 0))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((5, 5), dtype=np.uint8))
+    M = cv2.moments(mask)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    return np.array([cx, cy])
+
+  def detect_blue(self, image):
+    mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((5, 5), dtype=np.uint8))
+    M = cv2.moments(mask)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    return np.array([cx, cy])
+
+  def detect_yellow(self, image):
+    mask = cv2.inRange(image, (0, 100, 100), (0, 255, 255))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((5, 5), dtype=np.uint8))
+    M = cv2.moments(mask)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    return np.array([cx, cy])
+
+  def detect_target(self, image):
+    """Finds the position of orange sphere
+
+    :param image:  matrix, width*height*3
+        The image captured by camera2
+    :return: array, [x, y]
+        The position of orange sphere in pixel (Top-left:[0 , 0], Right-down:[width, height])
+    """
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(image, (11, 43, 46), (25, 255, 255))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((5, 5), dtype=np.uint8))
+    cv2.imshow('window 3', mask)
+    cv2.waitKey(2)
+
+
+
+  # Recieve data, process it, and publish
+  def callback2(self,data):
+    # Recieve the image
+    try:
+      self.cv_image2 = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+    # Uncomment if you want to save the image
+    #cv2.imwrite('image_copy.png', cv_image)
+    im2=cv2.imshow('window2', self.cv_image2)
+    cv2.waitKey(1)
+
+    self.detect_target(self.cv_image2)
+    # Publish the results
+    try: 
+      self.image_pub2.publish(self.bridge.cv2_to_imgmsg(self.cv_image2, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+# call the class
+def main(args):
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+# run the code if the node is called
+if __name__ == '__main__':
+    main(sys.argv)
+
+
