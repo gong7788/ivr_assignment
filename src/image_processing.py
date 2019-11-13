69ffb99b2592ed2c0282a8d9d30c5c925b676746
dekucrow <515057712@qq.com>
2019-11-13 16:43:41 +0000

    finish detect target and plot the figures

--- src/image_processing.py
+++ src/image_processing.py
@@ -17,8 +17,7 @@ class image_converter:
         # initialize the node named image_processing
         rospy.init_node('listener', anonymous=True)
 
-        self.target_position_xz = None
-        self.target_position_yz = None
+        self.target_pub = rospy.Publisher('/target/detected', Float64MultiArray, queue_size=10)
 
         self.joints_pub = rospy.Publisher("joints_pos", Float64MultiArray, queue_size=10)
         self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
@@ -46,7 +45,7 @@ class image_converter:
         # print (vector1.dot(vector2)/Lx*Ly)
         return np.arccos(vector1.dot(vector2)/(Lx*Ly))
 
-    def find_angles(self, xz_pos, yz_pos):
+    def find_angles(self, centers):
         """Finds joints angles
            The robot is redundant, so multiple answers for joints, ignore joint2
             :param xz_pos: list, 5 positions in xz plane [red, green, blue, yellow, target]
@@ -54,12 +53,7 @@ class image_converter:
             :return angles
         """
         # centers = [red, green, blue, yellow, target] -- red:[x,y,z] ...
-        centers = []
-        for i in range(5):
-            x = xz_pos[2 * i]
-            y = yz_pos[2 * i]
-            z = (xz_pos[2 * i + 1] + yz_pos[2 * i + 1])/2
-            centers.append(np.array([x, y, z]))
+
         vector_up = centers[0] - centers[1]
         vector_middle = centers[1] - centers[2]
         vector_down = centers[2] - centers[3]
@@ -76,20 +70,40 @@ class image_converter:
         joint1 = self.angle_between_vectors(proj, x_axis)
         return [joint1, joint2, joint3, joint4]
 
-    def target_coordinates(self, target, base):
+    def target_coordinates(self, target, base, blue):
         """Transforms pixel coordinates into meters
 
             :param target: [x,y,z] target coordinates in pixel
             :param base:   [x,y,z] base(yellow joint) coordinates in pixel
+            :param blue:   [x,y,z] blue joint coordinates in pixel
             :return        [x,y,z] coordinates in meters
         """
+        distance = np.linalg.norm(blue - base)
+        target_base = (2 / distance) * (target - base)
+        target_base[2] = -target_base[2]
+        return target_base
+
+
 
     def callback(self, xz_pos, yz_pos):
-        joint1 = self.find_angles(xz_pos.data, yz_pos.data)[0]
-        joint2 = self.find_angles(xz_pos.data, yz_pos.data)[1]
-        joint3 = self.find_angles(xz_pos.data, yz_pos.data)[2]
-        joint4 = self.find_angles(xz_pos.data, yz_pos.data)[3]
-        print('joint1:{:.2f}, joint2:{:.2f}, joint3:{:.2f}, joint4:{:.2f}'.format(joint1, joint2, joint3, joint4))
+        centers = []
+        for i in range(5):
+            x = xz_pos.data[2 * i]
+            y = yz_pos.data[2 * i]
+            z = (xz_pos.data[2 * i + 1] + yz_pos.data[2 * i + 1]) / 2
+            centers.append(np.array([x, y, z]))
+
+        joint1 = self.find_angles(centers)[0]
+        joint2 = self.find_angles(centers)[1]
+        joint3 = self.find_angles(centers)[2]
+        joint4 = self.find_angles(centers)[3]
+        # print('joint1:{:.2f}, joint2:{:.2f}, joint3:{:.2f}, joint4:{:.2f}'.format(joint1, joint2, joint3, joint4))
+        blue = centers[-3]
+        yellow = centers[-2]
+        target = centers[-1]
+        # print(self.target_coordinates(target, yellow, blue))
+        target_pub = Float64MultiArray()
+        target_pub.data = self.target_coordinates(target, yellow, blue)
 
 
         #control the robot joints
@@ -103,13 +117,14 @@ class image_converter:
         # self.joint4.data = 1
 
         # Publish the results
-        # try:
-        #     self.robot_joint1_pub.publish(self.joint1)
-        #     self.robot_joint2_pub.publish(self.joint2)
-        #     self.robot_joint3_pub.publish(self.joint3)
-        #     self.robot_joint4_pub.publish(self.joint4)
-        # except CvBridgeError as e:
-        #     print(e)
+        try:
+            # self.robot_joint1_pub.publish(self.joint1)
+            # self.robot_joint2_pub.publish(self.joint2)
+            # self.robot_joint3_pub.publish(self.joint3)
+            # self.robot_joint4_pub.publish(self.joint4)
+            self.target_pub.publish(target_pub)
+        except CvBridgeError as e:
+            print(e)
 
 
 # call the class
