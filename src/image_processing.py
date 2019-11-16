8caf506373243ef6b499439e16ff59d6ce417be5
dekucrow <515057712@qq.com>
2019-11-16 21:03:09 +0000

    solve angle calculation

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import roslib
+from scipy.optimize import leastsq
+import math
 import sys
 import rospy
 import cv2
@@ -33,24 +35,33 @@ class image_converter:
     #TODO finds joints
     #TODO transfer to meters coordinate
 
+    def fun_trans(self, x, data):
+        return ((3 * np.cos(x[2]) * np.sin(x[0]) * np.sin(x[1]) + 3 * np.cos(x[0]) * np.sin(x[2]) - data[0]),
+                (-3 * np.cos(x[0]) * np.cos(x[2]) * np.sin(x[1]) + 3 * np.sin(x[0]) * np.sin(x[2]) - data[1]),
+                (2 + 3 * np.cos(x[1]) * np.cos(x[2]) - data[2]),
+                (2 + 3 * np.cos(x[1]) * np.cos(x[2]) + 2 * np.cos(x[1]) * np.cos(x[2]) * np.cos(x[3]) - 2 * np.sin(x[1]) * np.sin(x[3]) - data[3]))
 
-    # def find_angles(self, centers):
+    def error(self, theta, true):
+        temp = self.fun_trans(theta)
+        estimated = np.array([temp[0][0], temp[1][0], temp[2][0], temp[3][0]])
+        return np.sum((estimated-true)**2)
+
+
+    def find_angles(self, end_effector, green):
     #     """Finds joints angles
     #        The robot is redundant, so multiple answers for joints, ignore joint2
     #         :param xz_pos: list, 5 positions in xz plane [red, green, blue, yellow, target]
     #         :param yz_pos: list, 5 positions in xz plane
     #         :return angles
     #     """
-    #     # centers = [red, green, blue, yellow, target] -- red:[x,y,z] ...
-    #     red_true = centers[0]
-    #     green_true = centers[1]
-    #     blue_true = centers[2]
-    #
-    #     a = [0, 0, 3, 2]
-    #     alpha = [1.57, 1.57, 1.57, 0]
-    #     d = [2, 0, 0, 0]
-
-
+        true = [green[0], green[1], green[2], end_effector[2]]
+        np.set_printoptions(suppress=True)
+        # res_1 = least_squares(self.error, x0=[0, 0, 0, 0],
+        #                       bounds=([-math.pi, -math.pi / 2, -math.pi / 2, -math.pi / 2], [math.pi, math.pi / 2, math.pi / 2, math.pi / 2]),
+        #                       args = true)
+        res_1 = leastsq(self.fun_trans, (0,0,0,0), args = true)
+        print(res_1[0])
+        return res_1[0]
 
 
     def target_coordinates(self, target, base, blue):
@@ -61,8 +72,7 @@ class image_converter:
             :param blue:   [x,y,z] blue joint coordinates in pixel
             :return        [x,y,z] coordinates in meters
         """
-        distance = np.linalg.norm(blue - base)
-        target_base = (2 / distance) * (target - base)
+        target_base = 0.0381 * (target - base)
         target_base[2] = -target_base[2]
         return target_base
 
@@ -88,10 +98,10 @@ class image_converter:
             Transform.append(T_theta.dot(T_alpha))
 
         red_co = Transform[0].dot(Transform[1]).dot(Transform[2]).dot(Transform[3])[0:3, -1]
-        # green_co = Transform[0].dot(Transform[1]).dot(Transform[2])[0:3, -1]
+        green_co = Transform[0].dot(Transform[1]).dot(Transform[2])[0:3, -1]
         # blue_co = Transform[0][0:3, -1]
 
-        return red_co
+        return [red_co, green_co]
 
     def callback(self, xz_pos, yz_pos):
         centers = []
@@ -113,13 +123,21 @@ class image_converter:
         target_pub = Float64MultiArray()
         target_pub.data = self.target_coordinates(target, yellow, blue)
 
-        input_theta = [2, 1, 1, 1]
+        input_theta = [0, 0, 1, 0]
         np.set_printoptions(suppress=True)
+
+
         true_end = np.array(self.target_coordinates(centers[0], yellow, blue))
-        FK_end = np.array(self.FK(input_theta))
+        green = np.array(self.target_coordinates(centers[1], yellow, blue))
+        estimate_angles = self.find_angles(true_end, green)
+        print("Estimated angles: ",estimate_angles)
+        print("Estimated coordinates: ",self.FK(estimate_angles)[0])
+        print("Error: ", np.sqrt(np.sum((self.FK(estimate_angles)[0]-true_end)**2)))
+        # FK_end = np.array(self.FK(input_theta)[0])
         print("True end effector: ", true_end)
-        print("FK end effector: ", FK_end)
-        print("Euclidean distance error: ", np.sqrt( np.sum((FK_end - true_end)**2)))
+        print("Green", green)
+        # print("FK end effector: ", FK_end)
+        # print("Euclidean distance error: ", np.sqrt( np.sum((FK_end - true_end)**2)))
         # print("Green: ", self.target_coordinates(centers[1], yellow, blue))
         # print("Blue: ", self.target_coordinates(centers[2], yellow, blue))
 
--- src/testsolve.py
+++ src/testsolve.py
@@ -1,16 +1,43 @@
 import sympy as sy
 import numpy as np
+from scipy.optimize import least_squares
+import math
 
-green_co = [0.03948031, -1.61869256,  4.61919585]
+# green_co = [0.03948031, -1.61869256,  4.61919585]
+#
+# green_X = green_co[0]
+# green_Y = green_co[1]
+# green_Z = green_co[2]
+#
+# angle1, angle2, angle3 = sy.symbols("angle1 angle2 angle3")
+#
+# eq = [3*sy.cos(angle3)*sy.sin(angle1)*sy.sin(angle2) + 3*sy.cos(angle1)*sy.sin(angle3) - green_X,
+#       -3*sy.cos(angle1)*sy.cos(angle3)*sy.sin(angle2) + 3*sy.sin(angle1)*sy.sin(angle3) - green_Y,
+#       2 + 3*sy.cos(angle2)*sy.cos(angle3) - green_Z]
+# result = sy.nonlinsolve(eq, [angle1, angle2, angle3])
+# print(result)
+def fun_trans(theta):
+    return np.array(
+        [[3 * np.cos(theta[2]) * np.sin(theta[0]) * np.sin(theta[1]) + 3 * np.cos(theta[0]) * np.sin(theta[2])],
+         [-3 * np.cos(theta[0]) * np.cos(theta[2]) * np.sin(theta[1]) + 3 * np.sin(theta[0]) * np.sin(theta[2])],
+         [2 + 3 * np.cos(theta[1]) * np.cos(theta[2])],
+         [2 + 3 * np.cos(theta[1]) * np.cos(theta[2]) + 2 * np.cos(theta[1]) * np.cos(theta[2]) * np.cos(theta[3]) - 2 * np.sin(theta[1]) * np.sin(theta[3])]])
 
-green_X = green_co[0]
-green_Y = green_co[1]
-green_Z = green_co[2]
+def error(theta, true):
+    temp = fun_trans(theta)
+    estimated = np.array([temp[0][0], temp[1][0], temp[2][0], temp[3][0]])
+    # print("estimated: ",estimated)
+    # print("\n green: ", green)
+    # print(temp)
+    # print(estimated)
+    return np.sum(((estimated-true)**2))
 
-angle1, angle2, angle3 = sy.symbols("angle1 angle2 angle3")
 
-eq = [3*sy.cos(angle3)*sy.sin(angle1)*sy.sin(angle2) + 3*sy.cos(angle1)*sy.sin(angle3) - green_X,
-      -3*sy.cos(angle1)*sy.cos(angle3)*sy.sin(angle2) + 3*sy.sin(angle1)*sy.sin(angle3) - green_Y,
-      2 + 3*sy.cos(angle2)*sy.cos(angle3) - green_Z]
-result = sy.nonlinsolve(eq, [angle1, angle2, angle3])
-print(result)
+
+green = [ 2.3241, -0.7239,  3.7338]
+red = [ 3.81  , -2.8575,  3.7338]
+
+true = np.array([[green[0], green[1], green[2], red[2]]])
+np.set_printoptions(suppress=True)
+res_1 = least_squares(error, x0=[0, 0, 0, 0], bounds=([-math.pi, -math.pi / 2, -math.pi / 2, -math.pi / 2], [math.pi, math.pi / 2, math.pi / 2, math.pi / 2]), args=true)
+print(res_1.x)
